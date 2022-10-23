4bc51c9427584b2d09282b78b5f04096252a6aa7
Lu, Kuang-Chen <lukuangchen1024@gmail.com>
2022-10-23 13:03:29 -0400

    done

--- week2/starter/src/p1_algos.rs
+++ week2/starter/src/p1_algos.rs
@@ -11,7 +11,11 @@
 ///
 /// Run `cargo test insort` to check your answers.
 pub fn insort(v: &mut Vec<i32>, n: i32) {
-  unimplemented!()
+    let mut i = 0;
+    while i < v.len() && v[i] < n {
+        i += 1;
+    }
+    v.insert(i, n);
 }
 
 type Node = i32;
@@ -22,7 +26,7 @@ type Node = i32;
 ///
 /// Note: in this graph representation, references to nodes are not e.g. indices into a vector, but actual
 /// Rust references. You need to be very careful when comparing two nodes for equality. For example, in the program:
-///   
+///
 ///    let x = 1; let y = 1;
 ///    assert!(&x == &y)
 ///
@@ -31,32 +35,51 @@ type Node = i32;
 ///
 /// Run `cargo test connected` to check your answers.
 pub fn connected(edges: &[(&Node, &Node)], src: &Node, dst: &Node) -> bool {
-  unimplemented!()
+    let mut visited_nodes: Vec<&Node> = vec![src];
+    fn is_visited(visited_nodes: &Vec<&Node>, node: &Node) -> bool {
+        for n in visited_nodes.iter() {
+            if std::ptr::eq(*n, node) {
+                return true;
+            }
+        }
+        return false;
+    }
+    let mut hopeful = true;
+    while hopeful && !is_visited(&visited_nodes, dst) {
+        hopeful = false;
+        for (src0, dst0) in edges {
+            if is_visited(&visited_nodes, src0) && !is_visited(&visited_nodes, dst0) {
+                hopeful = true;
+                visited_nodes.push(dst0);
+            }
+        }
+    }
+    is_visited(&visited_nodes, dst)
 }
 
 #[cfg(test)]
 mod test {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn insort_test() {
-    let mut v = vec![1, 5, 8];
+    #[test]
+    fn insort_test() {
+        let mut v = vec![1, 5, 8];
 
-    insort(&mut v, 0);
-    assert_eq!(v, vec![0, 1, 5, 8]);
+        insort(&mut v, 0);
+        assert_eq!(v, vec![0, 1, 5, 8]);
 
-    insort(&mut v, 3);
-    assert_eq!(v, vec![0, 1, 3, 5, 8]);
+        insort(&mut v, 3);
+        assert_eq!(v, vec![0, 1, 3, 5, 8]);
 
-    insort(&mut v, 9);
-    assert_eq!(v, vec![0, 1, 3, 5, 8, 9]);
-  }
+        insort(&mut v, 9);
+        assert_eq!(v, vec![0, 1, 3, 5, 8, 9]);
+    }
 
-  #[test]
-  fn connected_test() {
-    let nodes = vec![1, 1, 1];
-    let edges = vec![(&nodes[0], &nodes[1]), (&nodes[1], &nodes[2])];
-    assert!(connected(&edges, &nodes[0], &nodes[2]));
-    assert!(!connected(&edges, &nodes[2], &nodes[0]))
-  }
+    #[test]
+    fn connected_test() {
+        let nodes = vec![1, 1, 1];
+        let edges = vec![(&nodes[0], &nodes[1]), (&nodes[1], &nodes[2])];
+        assert!(connected(&edges, &nodes[0], &nodes[2]));
+        assert!(!connected(&edges, &nodes[2], &nodes[0]))
+    }
 }
--- week2/starter/src/p2_design.rs
+++ week2/starter/src/p2_design.rs
@@ -12,25 +12,25 @@
 ///   * v: representing a collection of numbers
 /// and rounds every number in-place in v to the nearest integer.
 pub fn round_all(
-  // (1) v could be a Vec<_>, &Vec<_>, &mut Vec<_>, &[_], or &mut[_]. I choose &mut[_] because
-  //     we do not need to change the size or order of the collection, but do need to change the elements.
-  // (2) v could be a &mut [{number type}], and "round to the nearest integer" implies the use of floats.
-  // (3) The choice of f32 vs. f64 is arbitrary -- we would need to use more advanced features to write one
-  //     function that works for both types, so we arbitrarily pick f32 for now.
-  v: &mut [f32],
+    // (1) v could be a Vec<_>, &Vec<_>, &mut Vec<_>, &[_], or &mut[_]. I choose &mut[_] because
+    //     we do not need to change the size or order of the collection, but do need to change the elements.
+    // (2) v could be a &mut [{number type}], and "round to the nearest integer" implies the use of floats.
+    // (3) The choice of f32 vs. f64 is arbitrary -- we would need to use more advanced features to write one
+    //     function that works for both types, so we arbitrarily pick f32 for now.
+    v: &mut [f32],
 )
 // No return value, since this function only mutates an input.
 {
-  for n in v.iter_mut() {
-    *n = n.round();
-  }
+    for n in v.iter_mut() {
+        *n = n.round();
+    }
 }
 
 #[test]
 fn round_all_test() {
-  let mut v = vec![0.3, 0.7];
-  round_all(&mut v);
-  assert_eq!(v, vec![0., 1.]);
+    let mut v = vec![0.3, 0.7];
+    round_all(&mut v);
+    assert_eq!(v, vec![0., 1.]);
 }
 
 // Now you try!
@@ -41,15 +41,42 @@ fn round_all_test() {
 /// and returns a value:
 ///   * representing which strings in the collection contain the needle
 pub fn find_contains(
-  /* Pick and justify your parameters */
-) -> () /* Pick and justify your return type */
+    /* Pick and justify your parameters */
+    // (1) haystack could be a Vec<_>, &Vec<_>, &mut Vec<_>, &[_], or &mut[_].
+    //     I choose &[_] because
+    //        we do not need to change the size or order of the collection,
+    //     and because
+    //        we also do not need to change the elements.
+    // (2) haystack could be a &[&str] or &[&String] or &[String].
+    //     I choose &[&str] because it is the most permissive option.
+    // (3) needle could be a &str or &String or String.
+    //     I choose &str because it is the most permissive option.
+    haystack: &[&str],
+    needle: &str,
+) -> Vec<String>
+/* Pick and justify your return type */
+    // (1) The return type could be Vec<String>. So I choose it.
+    //     Types with `&` are not options because Rust will ask me to specify the "lifetime", which is a concept that I don't understand.
+    //     Types with `[_]` are not options because the size depends on the inputs.
 {
-  unimplemented!();
+    let mut result = vec![];
+    for s in haystack.iter() {
+        if s.contains(needle) {
+            result.push(String::from(*s));
+        }
+    }
+    return result;
 }
 
 #[test]
 fn find_contains_test() {
-  /* Add your unit test here! */
+    /* Add your unit test here! */
+    assert_eq!(
+        find_contains(&["hello", "world"], "l"),
+        vec!["hello", "world"]
+    );
+    assert_eq!(find_contains(&["hello", "world"], "ld"), vec!["world"]);
+    assert_eq!(find_contains(&["hello", "world"], "ll"), vec!["hello"]);
 }
 
 /// P2b: fill_progress_bar is a function that takes:
@@ -60,13 +87,47 @@ fn find_contains_test() {
 /// For example, at a progress of 20% with bracketed delimiters, the bar would be:
 ///   [==        ]
 pub fn fill_progress_bar(
-  /* Pick and justify your parameters */
-) -> () /* Pick and justify your return type */
+    /* Pick and justify your parameters */
+    // (1) buf could be a &mut String or &mut str.
+    //     I choose &mut String because I don't know how to add content to an &mut str.
+    //     Types without &mut are not options because we need to change the *same* buf.
+    // (2) delims could be a pair of String or str or &str or &String or &mut str or &mut String.
+    //     I choose a pair of &str because it is the most permissive option -- we need neither the ownership nor the "write access".
+    // (3) frac could be {number type} or &{number type}.
+    //     I choose {number type} because number types are freely copiable. Furthermore, copying them is sometimes cheaper
+    //     than copying their references.
+    // (4) frac could be f32 and f64.
+    //     The choice of f32 vs. f64 is arbitrary -- we would need to use more advanced features to write one
+    //     function that works for both types, so we arbitrarily pick f32 for now.
+    buf: &mut String,
+    delims: (&str, &str),
+    frac: f32,
+) -> ()
+/* Pick and justify your return type */
+    // (1) The return type could be anything.
+    //     I choose the unit type to remind the users of this function that calling this function has a side effect.
 {
-  unimplemented!();
+    let total = buf.len();
+    buf.clear();
+    let (lft, rht) = delims;
+    let barlen = total - lft.len() - rht.len();
+    let n: usize = (barlen as f32 * frac).round() as usize;
+    let m: usize = barlen - n;
+    let fll = "=".repeat(n);
+    let spc = " ".repeat(m);
+    buf.push_str(lft);
+    buf.push_str(&fll[..]);
+    buf.push_str(&spc[..]);
+    buf.push_str(rht)
 }
 
 #[test]
 fn test_fill_progress_bar() {
-  /* Add your unit test here! */
+    let mut s = String::from("[==        ]");
+    fill_progress_bar(&mut s, ("[", "]"), 1.0);
+    assert_eq!(s, "[==========]");
+    fill_progress_bar(&mut s, ("<", ">"), 0.0);
+    assert_eq!(s, "<          >");
+    fill_progress_bar(&mut s, ("{", "}"), 0.2);
+    assert_eq!(s, "{==        }");
 }
